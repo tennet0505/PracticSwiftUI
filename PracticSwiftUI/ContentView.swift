07f5d6fd904f52e7fe8ac6151d44f173b12472bf
Ten Ol <tennet0505@gmail.com>
2020-03-06 23:11:22 +0600

    Initial Commit

---
+++ PracticSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticSwiftUI
+//
+//  Created by Oleg Ten on 3/6/20.
+//  Copyright © 2020 Oleg Ten. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
